83f08fa484468746c74ed9c25beb0806d7d78414
Syeda Maryam Ahmed <smaryamahmed2006@gmail.com>
2024-06-26 21:47:49 +0500

    Initial commit

--- CLINT/vite-project/src/Constant/index.jsx
+++ CLINT/vite-project/src/Constant/index.jsx
@@ -1,3 +1,11 @@
-export const BASE_URL1 = "http://localhost:3000/api/auth"
-export const BASE_URL2 = "http://localhost:3000/api/form"
-export const BASE_URL3 = "http://localhost:3000/api/forms"
+// export const BASE_URL1 = "http://localhost:3000/api/auth"
+// export const BASE_URL2 = "http://localhost:3000/api/form"
+// export const BASE_URL3 = "http://localhost:3000/api/forms"
+
+// constant.jsx
+
+// constant.jsx
+
+export const BASE_URL1 = import.meta.env.VITE_REACT_APP_BASE_URL1 || 'http://localhost:3000/api/auth';
+export const BASE_URL2 = import.meta.env.VITE_REACT_APP_BASE_URL2 || 'http://localhost:3000/api/form';
+export const BASE_URL3 = import.meta.env.VITE_REACT_APP_BASE_URL3 || 'http://localhost:3000/api/forms';
